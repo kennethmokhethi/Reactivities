c3e3036991e0d4115b8e1c8b53fb39b8fce9813e
kenneth Mahlatse <kennethmahlatsemokhethi@gmail.com>
2024-05-12 13:55:28 +0200

    end of section 9

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app/layout/App'
+import 'react-calendar/dist/Calendar.css'
 import 'semantic-ui-css/semantic.min.css'
 import './app/layout/styles.css'
 import { StoreContext, store } from './app/stores/store'
